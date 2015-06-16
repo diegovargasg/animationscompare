0cd26082f7f1747b15229535aca3deef0a7dd7b1
diegovargasg <diegodevelopero@gmail.com>
2015-06-16 19:22:19 +0200

    Built backboneapp from commit cbb84ed on branch master

--- scripts/templates.js
+++ scripts/templates.js
@@ -6,7 +6,7 @@ this["JST"]["app/scripts/templates/aside.ejs"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<div class="checkbox-wrapper">\n\t<div class="checkbox">\n\t\t<label for="css3"><input id="css3" type="radio" data-type="css3" name="method[]" checked class="" /> CSS3</label>\n\t</div>\n\t<div class="checkbox">\n\t\t<label for="js"><input id="js" type="radio" data-type="js" name="method[]" class="" /> Js</label>\n\t</div>\n\t<div class="checkbox">\n\t\t<label for="jquery"><input id="jquery" type="radio" data-type="jquery" name="method[]" class="" /> jQuery</label>\n\t</div>\n\t<div class="checkbox">\n\t\t<label for="svg"><input id="svg" type="radio" data-type="svg" name="method[]" /> svg</label>\n\t</div>\n</div>\n<div class="form-group">\n\t<label class="control-label">Number of Elements: <span id="num-el-text">1</span></label>\n\t<input type="range" id="num-el" min="1" value="1" max="50">\n</div>\n<div class="input-wrap">\n\t<button id="run-stop" class="btn btn-default"></button>\n</div>\n';
+__p += '<div class="panel panel-default">\n  <div class="panel-heading">\n    <h3 class="panel-title">Settings</h3>\n  </div>\n  <div class="panel-body">\n    <div class="checkbox-wrapper">\n\t\t\t<div class="checkbox">\n\t\t\t\t<label for="css3"><input id="css3" type="radio" data-type="css3" name="method[]" checked class="" /> CSS3</label>\n\t\t\t</div>\n\t\t\t<div class="checkbox">\n\t\t\t\t<label for="js"><input id="js" type="radio" data-type="js" name="method[]" class="" /> Js</label>\n\t\t\t</div>\n\t\t\t<div class="checkbox">\n\t\t\t\t<label for="jquery"><input id="jquery" type="radio" data-type="jquery" name="method[]" class="" /> jQuery</label>\n\t\t\t</div>\n\t\t\t<div class="checkbox">\n\t\t\t\t<label for="svg"><input id="svg" type="radio" data-type="svg" name="method[]" /> svg</label>\n\t\t\t</div>\n\t\t</div>\n\t\t<div class="form-group">\n\t\t\t<label class="control-label">Number of Elements: <span id="num-el-text">1</span></label>\n\t\t\t<input type="range" id="num-el" min="1" value="1" max="50">\n\t\t</div>\n\t\t<div class="input-wrap">\n\t\t\t<button id="run-stop" class="btn btn-default"></button>\n\t\t</div>\n  </div>\n</div>';
 
 }
 return __p
@@ -16,17 +16,7 @@ this["JST"]["app/scripts/templates/content.ejs"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<p>Main Section</p>\n<div id="main-section-wrap"></div>';
-
-}
-return __p
-};
-
-this["JST"]["app/scripts/templates/footer.ejs"] = function(obj) {
-obj || (obj = {});
-var __t, __p = '', __e = _.escape;
-with (obj) {
-__p += '<p>Footer</p>\n\n';
+__p += '<div class="panel panel-default">\n  <div class="panel-heading">\n    <h3 class="panel-title">Result</h3>\n  </div>\n  <div class="panel-body">\n    <div id="main-section-wrap">\n    \t<p class="initial-message">No settings selected yet.</p>\n    </div>\n  </div>\n</div>';
 
 }
 return __p
@@ -36,7 +26,7 @@ this["JST"]["app/scripts/templates/header.ejs"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<p>Header</p>\n\n';
+__p += '<nav class="navbar navbar-default navbar-fixed-top">\n  <div class="container">\n    <a href="#" class="navbar-brand">Simple Animations Compare</a>\n  </div>\n</nav>';
 
 }
 return __p
